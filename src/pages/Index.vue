9b0ab8e059a06b2166651873c0c7a354d694f6c8
mich <843298603@qq.com>
2019-06-12 14:20:24 +0800

    封装api接口-auth&blog

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,9 +10,14 @@
 </template>
 
 <script>
-import request from "@/helpers/request.js";
+import request from "@/helpers/request"
+import auth from '@/api/auth'
+import blog from '@/api/blog'
 
 window.request = request
+window.auth = auth
+window.blog = blog
+
 
 export default {
   data () {
