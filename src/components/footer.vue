02a0c1ec9531ab84846ef794f590d83e4003a408
mich <843298603@qq.com>
2019-06-12 20:11:52 +0800

    项目结构雏形完成

---
+++ src/components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <footer>
+        <p>© 2019 https://github.com/Michcola</p>
+    </footer>
+</template>
+
+<style lang="less" scope>
+
+footer {
+    align-self: end;
+    background-color: #d7d7d7;
+    color: #666;
+    font-size: 13px;
+    padding: 10px;
+    text-align: center;
+}
+
+</style>
