47589b476abed78ec3750a5ecf7081671481f3f8
mich <843298603@qq.com>
2019-06-15 17:42:32 +0800

    新增edit的html及样式

--- src/pages/Edit.vue
+++ src/pages/Edit.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="edit">
-   edit
+  <div id="edit">
+    <h1>编辑文章</h1>
+    <h3>文章标题</h3>
+    <el-input v-model="title" placeholder="输入您的标题" maxlength="20"></el-input>
+    <p class="msg"><span>{{titleMax}}/20</span></p>
+    <h3>内容简介</h3>
+    <!-- 自适应高度组件：添加 autosize 属性 -->
+    <el-input type="textarea" v-model="description" placeholder="展示博客简介" maxlength="100"></el-input>
+    <p class="msg"><span>{{descriptMax}}/100</span></p>
+    <h3>文章内容</h3>
+    <el-input type="textarea" v-model="content" placeholder="请输入内容"></el-input>
+    <p class="msg"><span>已输入{{contentLength}}个字</span></p>
+    <!-- 添加 atIndex 开关 -->
+    <p>
+      <label>是否展示到首页</label>
+      <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+    </p>
+    <el-button >确定</el-button>
   </div>
 </template>
 
@@ -16,7 +32,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.edit{
-  color:red;
-}
+  p.msg {
+    display: grid;
+    justify-content: end;
+    color: #999;
+    font-size: 12px;
+  }
 </style>
