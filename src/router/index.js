6b9dfb0b62d25ad82f7a2053f558a5b399af055b
mich <843298603@qq.com>
2019-06-15 20:18:28 +0800

    路由完善,异步加载,权限控制

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,26 @@
+
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/pages/Index'
-import Login from "@/pages/Login"
-import Register from "@/pages/Register"
-import Create from "@/pages/Create"
-import Detail from "@/pages/Detail"
-import Edit from "@/pages/Edit"
-import My from "@/pages/My"
-import User from "@/pages/User"
+/*
+import Index from '@/pages/Index/template.vue'
+import Create from '@/pages/Create/template.vue'
+import User from '@/pages/User/template.vue'
+import Detail from '@/pages/Detail/template.vue'
+import My from '@/pages/My/template.vue'
+import Login from '@/pages/Login/template.vue'
+import Register from '@/pages/Register/template.vue'
+import Edit from '@/pages/Edit/template.vue'*/
+import store from '../store'
 
 Vue.use(Router)
 
-export default new Router({
+
+// window.store = store
+//
+// Vue.use(Router)
+
+/*
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -27,23 +36,92 @@ export default new Router({
     },
     {
       path: '/create',
-      component: Create
+      component: Create,
+      meta: {requiresAuth: true}
     },
     {
-      path: '/detail',
+      path: '/detail/:blogId',
       component: Detail
     },
     {
-      path: '/edit',
-      component: Edit
+      path: '/edit/:blogId',
+      component: Edit,
+      meta: {requiresAuth: true}
     },
     {
       path: '/my',
-      component: My
+      component: My,
+      meta: {requiresAuth: true}
+
     },
     {
-      path: '/user',
+      path: '/user/:userId',
       component: User
     }
   ]
 })
+
+*/
+
+const router =  new Router({
+  routes: [
+    {
+      path: '/',
+      // 回调函数的异步加载
+      component: () => import('@/pages/Index.vue')
+    },
+    {
+      path: '/register',
+      component: () => import('@/pages/Register.vue')
+    },
+    {
+      path: '/login',
+      component: () => import('@/pages/Login.vue')
+    },
+    {
+      path: '/user/:userId',
+      component: () => import('@/pages/User.vue')
+    },
+    {
+      path: '/detail/:blogId',
+      component: () => import('@/pages/Detail.vue')
+    },
+    {
+      path: '/edit/:blogId',
+      component: () => import('@/pages/Edit.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/create',
+      component: () => import('@/pages/Create.vue'),
+      // 配置 meta 字段：路由元信息
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/my',
+      component: () => import('@/pages/My.vue'),
+      meta: { requiresAuth: true }
+    }
+  ]
+})
+
+// 访问这个 meta 字段
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    store.dispatch('checkLogin').then(isLogin => {
+      if (!isLogin) {
+        next({
+          path: '/login',
+          query: { redirect: to.fullPath }
+        })
+      } else {
+        next()
+      }
+      })
+  } else {
+    next() // 确保一定要调用 next()
+  }
+})
+
+export default router;
