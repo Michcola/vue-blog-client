991bc2fac097c23959e3378254eb4bef8131c7f1
mich <843298603@qq.com>
2019-06-15 17:46:53 +0800

    修改create的html及样式

--- src/pages/Create.vue
+++ src/pages/Create.vue
@@ -2,21 +2,21 @@
   <div id="create">
     <h1>创建文章</h1>
     <h3>文章标题</h3>
-    <el-input v-model="title" placeholder="输入您的标题" maxlength="20"></el-input>
-    <p class="msg"><span>{{titleMax}}/20</span></p>
+    <el-input placeholder="输入您的标题" maxlength="20"></el-input>
+    <p class="msg"><span>123</span></p>
     <h3>内容简介</h3>
     <!-- 自适应高度组件：添加 autosize 属性 -->
-    <el-input type="textarea" v-model="description" placeholder="展示博客简介" maxlength="100"></el-input>
-    <p class="msg"><span>{{descriptMax}}/100</span></p>
+    <el-input type="textarea" placeholder="展示博客简介" maxlength="100"></el-input>
+    <p class="msg"><span>123/100</span></p>
     <h3>文章内容</h3>
-    <el-input type="textarea" v-model="content" placeholder="请输入内容"></el-input>
-    <p class="msg"><span>已输入{{contentLength}}个字</span></p>
+    <el-input type="textarea" placeholder="请输入内容"></el-input>
+    <p class="msg"><span>已输入444个字</span></p>
     <!-- 添加 atIndex 开关 -->
     <p>
       <label>是否展示到首页</label>
-      <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+      <el-switch active-color="#13ce66" inactive-color="#ff4949"></el-switch>
     </p>
-    <el-button @>确定</el-button>
+    <el-button >确定</el-button>
   </div>
 </template>
 
