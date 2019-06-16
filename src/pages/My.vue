a8fae605fa852ba8906ddd0f42d305605b30063e
mich <843298603@qq.com>
2019-06-16 11:58:39 +0800

    完善my页

--- src/pages/My.vue
+++ src/pages/My.vue
@@ -1,122 +1,160 @@
 <template>
   <div id="my">
     <section class="user-info">
-     <img class="avatar" src="http://cn.gravatar.com/avatar/1?s=128&d=identicon" alt="">
-     <h3>嘻嘻</h3>
+      <img :src="user.avatar" :alt="user.username" class="avatar">
+      <h3>{{user.username}}</h3>
     </section>
     <section>
-      <div class="item">
+      <router-link class="item" v-for="blog in blogs" :key="blog.id" :to="`/detail/${blog.id}`">
         <div class="date">
-          <span class="day">20</span>
-          <span class="month">5月</span>
-          <span class="year">2018</span>
+          <span class="day">{{splitDate(blog.createdAt).date}}</span>
+          <span class="month">{{splitDate(blog.createdAt).month}}</span>
+          <span class="year">{{splitDate(blog.createdAt).year}}</span>
         </div>
-        <h3>前端异步揭秘 哦</h3>
-        <p>asdasdasdadadasdasdasdasd</p>
+        <h3>{{blog.title}}</h3>
+        <p>{{blog.description}}</p>
         <div class="actions">
-          <router-link to="/edit">编辑</router-link>
-          <a href="#">删除</a>
+          <router-link :to="`/edit/${blog.id}`">编辑</router-link>
+          <!-- 通过 preventDefault 阻止页面闪动 -->
+          <a href="#" @click.prevent="onDelete(blog.id)">删除</a>
         </div>
-      </div>
-
-      <div class="item">
-        <div class="date">
-          <span class="day">20</span>
-          <span class="month">5月</span>
-          <span class="year">2018</span>
-        </div>
-        <h3>前端异步揭秘 哦</h3>
-        <p>asdasdasdadadasdasdasdasd</p>
-        <div class="actions">
-          <router-link to="/edit">编辑</router-link>
-          <a href="#">删除</a>
-        </div>
-      </div>
-
-
+      </router-link>
+    </section>
+    <section class="pagination">
+      <el-pagination
+        @current-change="onPageChange"
+        layout="prev, pager, next"
+        :current-page="page"
+        :total="total">
+      </el-pagination>
     </section>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import blog from '@/api/blog.js'
+  // 通过 vuex 组件获取 我的 的 user
+  import { mapGetters } from 'vuex'
+
+  export default {
+    data() {
+      return {
+        blogs: [],
+        page: 1,
+        total: 0
+      }
+    },
+
+    computed: {
+      ...mapGetters(['user'])
+    },
+
+    created() {
+      this.page = parseInt(this.$route.query.page) || 1
+      blog.getBlogsByUserId(this.user.id, { page: this.page })
+        .then(res => {
+          // console.log(res)
+          this.blogs = res.data
+          this.total = res.total
+          this.page = res.page
+        })
+    },
+
+    methods: {
+      splitDate(dataStr) {
+        let dateObj = typeof dataStr === 'object' ? dataStr : new Date(dataStr)
+        return {
+          date: dateObj.getDate(),
+          // getMouth() 返回值是 0（一月） 到 11（十二月）的一个整数
+          month: dateObj.getMonth() + 1,
+          year: dateObj.getFullYear()
+        }
+      },
+
+      // 引入 弹框组件
+      async onDelete(blogId) {
+        await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        })
+        await
+          blog.deleteBlog({ blogId })
+        this.$message.success('删除成功!')
+        // 从数组里面过滤出已删除的博客，达到不用刷新页面就去掉已删除博客的优化
+        this.blogs = this.blogs.filter(blog => blog.id != blogId)
+      },
+
+      onPageChange(newPage) {
+        blog.getBlogsByUserId(this.user.id,{ page: newPage })
+          .then(res => {
+            this.blogs = res.data
+            this.total = res.total
+            this.page = res.page
+            this.$router.push({ path: "/my",query: {page: newPage }})
+          })
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-@import "../assets/base.less";
+  @import "../assets/base";
 
-#my,#user {
-  .user-info {
-    display: grid;
-    grid: auto auto / 80px 1fr;
-
-    margin-top: 30px;
-    padding-bottom: 20px;
-    border-bottom: 1px solid #ebebeb;
-
-    .avatar {
-      grid-column: 1;
-      grid-row: 1 / span 2;
-      width: 60px;
-      height: 60px;
-      border-radius: 50%;
-    }
-
-    h3 {
-      grid-column: 2;
-      grid-row: 1;
-      margin-top: 10px;
-    }
-  }
-
-  .item {
-    display: grid;
-    grid: auto auto auto / 80px 1fr;
-    margin: 20px 0;
-
-    .date {
-      grid-column: 1px;
-      grid-row: 1 / span 3;
-      justify-self: center;
-      text-align: center;
-
-      span {
-        display: block;
-        color: @textLighterColor;
+  #my{
+    .user-info {
+      display: grid;
+      grid: auto auto / 80px 1fr;
+      margin-top: 30px;
+      padding-bottom: 20px;
+      border-bottom: 1px solid #ebebeb;
+      .avatar {
+        grid-column: 1;
+        grid-row: 1 / span 2;
+        width: 60px;
+        height: 60px;
       }
-
-      .day {
-        font-size: 40px;
+      h3 {
+        grid-column: 2;
+        grid-row: 1;
+        margin-top: 10px;
       }
     }
-
-    h3 {
-      grid-column: 2;
-      grid-row: 1;
-    }
-
-    p {
-      grid-column: 2;
-      grid-row: 2;
-      margin-top: 0;
-    }
-
-    .actions {
-      grid-column: 2;
-      grid-row: 3;
-      font-size: 12px;
-
-      a {
-        color: @themeLighterColor;
+    .item {
+      display: grid;
+      grid: auto auto auto / 80px 1fr;
+      margin: 20px 0;
+      .date {
+        grid-column: 1;
+        grid-row: 1 / span 3;
+        justify-self: center;
+        text-align: center;
+        span {
+          display: block;
+          color: @textLighterColor;
+        }
+        .day {
+          font-size: 40px;
+        }
+      }
+      h3 {
+        grid-column: 2;
+        grid-row: 1;
+      }
+      p {
+        grid-column: 2;
+        grid-row: 2;
+        margin-top: 0;
+      }
+      .actions {
+        grid-column: 2;
+        grid-row: 3;
+        font-size: 12px;
+        a {
+          color: @themeLighterColor;
+        }
       }
     }
   }
-}
 </style>
